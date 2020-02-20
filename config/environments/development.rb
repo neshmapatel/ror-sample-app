f4d0ff556874323824f7c5a3f06c8f9b2a700ab9
neshmapatel1399@gmail.com <neshma.patel@NeshmaPatel.local>
2020-02-20 17:04:51 +0530

    init

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,11 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000'                     # Local server
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  
 
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
@@ -27,10 +32,15 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  
 
-  config.action_mailer.perform_caching = false
 
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000'                     # Local server
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
