d8d2b04454d566af1bcb2cc15ed9b21b2e4e0e8b
neshmapatel1399@gmail.com <neshma.patel@NeshmaPatel.local>
2020-02-21 15:21:30 +0530

    admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,22 @@
 User.create!(name:  "Neshma Patel",
-             email: "neshmapatel@gmail.com",
+             email: "neshmapatel@gmail.org",
              password:              "111111",
              password_confirmation: "111111",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
 
-
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
+end
 
 users = User.order(:created_at).take(6)
 50.times do
